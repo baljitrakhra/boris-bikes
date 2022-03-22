1c5cf39b11026ec8b49f0755253cb628eb31378c
Conor Hope <c_hope@hotmail.co.uk>
2022-03-22 21:07:11 +0000

    Completed section 10

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,4 +5,8 @@ describe DockingStation do
     docking_station = DockingStation.new
     expect(docking_station.release_bike).to be_an_instance_of(Bike)
   end
+  it "working? method returns true" do
+    bike = Bike.new
+    expect(bike.working?).to be_truthy
+  end
 end
