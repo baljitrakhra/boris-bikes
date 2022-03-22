634756dc9f4136ebb9d2660cb5dd224c30e9f5ab
Coral Baker <coral-ann@Corals-MacBook-Air.local>
2022-03-22 16:40:51 +0000

    Added method test to docking_station_spec.rb.

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,8 @@
 require 'boris_bike'
 
 describe DockingStation do
-  
+  it 'looks for method release_bike' do
+    # docking_station = DockingStation.new
+    expect(release_bike).to eq 'true'
+  end
 end
