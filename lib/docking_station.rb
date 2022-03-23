dcb56924246a54bdca55fbf33e52e00eebc565b8
Coral Baker <coral-ann@Corals-MacBook-Air.local>
2022-03-23 16:42:50 +0000

    Added tests & methods for docking.

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,8 +1,17 @@
+require 'bike.rb'
+
 class DockingStation
+  attr_reader :bike # Shorthand for def bike!
+
   def release_bike
-     bike = Bike.new
+    Bike.new
   end
+
+  def dock(bike)
+    @bike = bike
+  end
+
+  # def bike
+  #   @bike
+  # end
 end
-
-
-
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,12 +1,25 @@
 require 'docking_station'
 
 describe DockingStation do
-  it 'looks for method release_bike' do
-    docking_station = DockingStation.new
-    expect(docking_station.release_bike).to be_an_instance_of(Bike)
+  it { is_expected.to respond_to :release_bike }
+
+  it "Releases working bikes" do
+    bike = subject.release_bike
+    expect(bike).to be_working
   end
-  it "working? method returns true" do
+
+  it { is_expected.to respond_to(:dock).with(1).argument }
+
+  it "docks something" do
     bike = Bike.new
-    expect(bike.working?).to be_truthy
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  it { is_expected.to respond_to(:bike) }
+
+  it "returns docked bikes" do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
   end
 end
