146bff4aab324c69a8c30f56ec222941dbd3d910
Baljit Rakhra <baljit.rakhra@gmail.com>
2022-03-24 15:57:26 +0000

    Chapter 12 completed

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,6 +4,7 @@ class DockingStation
   attr_reader :bike # Shorthand for def bike!
 
   def release_bike
+    fail 'No bikes available' unless @bike
     @bike
   end
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,31 +2,35 @@ require 'docking_station'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
-
   describe '#release_bike' do
     it "releases a bike" do
       bike = Bike.new
       subject.dock(bike)
       expect(subject.release_bike).to eq bike
-    end 
+    end
+  end
+  describe '#release_bike' do
+    it 'raises an error when there are no bikes available' do
+      # Let's not dock a bike first:
+      # remember subject == DockingStation.new
+      expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
   end
 
-  
   it { is_expected.to respond_to(:dock).with(1).argument }
-  
-
   it { is_expected.to respond_to(:bike) }
-
   it "docks something" do
     bike = Bike.new
     expect(subject.dock(bike)).to eq bike
   end
-
   it "returns docked bikes" do
     bike = Bike.new
     subject.dock(bike)
     expect(subject.bike).to eq bike
   end
-
 end
+
+
+
+
 
