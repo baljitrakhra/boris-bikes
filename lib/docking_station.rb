33b0257b7c4e045ae9dc1ffceab6ad86a278a7ef
Ruzeka Uddin <ruzekauddin@Ruzekas-MacBook-Air.local>
2022-03-25 10:50:05 +0000

    Single Responsibility Principle

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -8,16 +8,25 @@ class DockingStation
   end
 
   def release_bike
-    fail 'No bikes available' if @bikes.empty?
+    fail 'No bikes available' if empty? # empty? is a private class method.
     @bikes.pop
   end
 
   def dock(bike)
-    fail 'Docking station full' if @bikes.count >= 20
+    fail 'Docking station full' if full? 
+    #if @bikes.count >= 20
     @bikes << bike
   end
 
-  # def bike
-  #   @bike
-  # end
+  private
+  
+  def full?
+    @bikes.count >= 20 ? true : false
+  end
+
+  def empty?
+    @bikes.empty? 
+  end 
 end
+
+
